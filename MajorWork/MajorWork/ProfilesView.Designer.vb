9dc9cdcf4938f9a2d643b35c1c09a0ccf14a9c64
OtherBarry <charlie.wilson4@gmail.com>
2016-05-17 19:36:13 +1000

    Accounting for long names and Rudimentary Highlighting.

--- MajorWork/MajorWork/ProfilesView.Designer.vb
+++ MajorWork/MajorWork/ProfilesView.Designer.vb
@@ -24,13 +24,16 @@ Partial Class ProfilesView
     Private Sub InitializeComponent()
         Me.TextBox1 = New System.Windows.Forms.RichTextBox()
         Me.RowerBox = New System.Windows.Forms.FlowLayoutPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(485, 12)
+        Me.TextBox1.Location = New System.Drawing.Point(485, 200)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(232, 367)
+        Me.TextBox1.Size = New System.Drawing.Size(232, 179)
         Me.TextBox1.TabIndex = 1
         Me.TextBox1.Text = ""
         '
@@ -42,19 +45,42 @@ Partial Class ProfilesView
         Me.RowerBox.Size = New System.Drawing.Size(467, 367)
         Me.RowerBox.TabIndex = 1
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 8.0!)
+        Me.Label1.Location = New System.Drawing.Point(4, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(136, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "SHIVAKUMAARUN, Kieran"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(534, 75)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(140, 50)
+        Me.Panel1.TabIndex = 2
+        '
         'ProfilesView
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(729, 391)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.RowerBox)
         Me.Controls.Add(Me.TextBox1)
         Me.Name = "ProfilesView"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents RowerBox As FlowLayoutPanel
     Friend WithEvents TextBox1 As RichTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
 End Class
--- MajorWork/MajorWork/ProfilesView.vb
+++ MajorWork/MajorWork/ProfilesView.vb
@@ -1,5 +1,13 @@
 ﻿Imports System.Data.OleDb
 Public Class ProfilesView
+    Private Sub RowerPanelEnter(sender As Object, e As EventArgs)
+        Dim entered As Panel = sender
+        entered.BackColor = skyOrange
+    End Sub
+    Private Sub RowerPanelExit(sender As Object, e As EventArgs)
+        Dim exited As Panel = sender
+        exited.BackColor = schoolBlue
+    End Sub
     Private Sub ProfilesView_Load(sender As Object, e As EventArgs) Handles Me.Load
         Dim SelectedID As Integer = 400052841
         Dim IDNum As New List(Of Int32)
@@ -8,15 +16,12 @@ Public Class ProfilesView
         Dim sName As New List(Of String)
         Dim yGroup As New List(Of String)
         Dim rClass As New List(Of String)
-        Using conn As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\rowingDatabase (1).accdb") 'add your access file to bin\debug and then repalce \athelte with \(name of your file)
-            conn.Open() 'open the connection to the database
-            Using cmd As New OleDbCommand("SELECT ID, sName, gName, rClass, Group FROM tbProfiles", conn) '*takes the column with correct rows
-                'cmd.Parameters.Add(New OleDbParameter("madeupVariable", "variableYouNeedToFind")) 'maps your variable to that string
-                Using dr = cmd.ExecuteReader() 'reads the database
-                    If dr.HasRows Then 'checks if there are records that fulfill your criteria
-                        Do While dr.Read() 'while loop that goes to eof
-                            '*your function to perform*
-                            '(access columns with dr(*index*) Or dr(*fieldName*))
+        Using conn As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\rowingDatabase (1).accdb")
+            conn.Open()
+            Using cmd As New OleDbCommand("SELECT ID, sName, gName, rClass, Group FROM tbProfiles", conn)
+                Using dr = cmd.ExecuteReader()
+                    If dr.HasRows Then
+                        Do While dr.Read()
                             TextBox1.Text += dr.GetInt32(0).ToString + "    " + dr.GetString(1) + " " + dr.GetString(2) & vbCrLf
                             IDNum.Add(dr.GetInt32(0))
                             IDStr.Add(dr.GetInt32(0).ToString)
@@ -29,45 +34,49 @@ Public Class ProfilesView
                 End Using
             End Using
         End Using
-        Dim i As Integer = 0
-        'For Each ID As String In IDStr
-        '    Dim lbl As New Label
-        '    lbl.Size = New System.Drawing.Size(159, 23) 'set your size (if required)
-        '    lbl.Location = New System.Drawing.Point(12, 180) 'set your location
-        '    lbl.Text = "label text goes here" 'set the text for your label
-        '    Me.Controls.Add(lbl)  'add your new control to your forms control collection
-        '    lbl.Name = "DspRow" + i.ToString
-        '    i = i + 1
-        'Next
-
-        For x As Integer = 0 To IDNum.Count - 1
+        For rower As Integer = 0 To IDNum.Count - 1
             Dim testPanel As New Panel With
                 {
                     .Height = 50,
                     .Width = 140,
                     .BackColor = schoolBlue,
-                    .Name = "rPanel" + x.ToString
+                    .Name = "rPanel" + rower.ToString
                 }
             Dim testID As New Label With
                 {
-                    .Text = IDStr(x),
+                    .Text = IDStr(rower),
                     .Font = New Drawing.Font("Segoe UI", 9.75, FontStyle.Bold),
                     .Location = New Point(0, 0),
-                    .Name = "rID" + x.ToString
+                    .Name = "rID" + rower.ToString
                 }
             Dim testName As New Label With
                 {
-                    .Text = gName(x).ToUpper + ", " + sName(x),
-                    .Font = New Drawing.Font("Segoe UI", 8.75),
+                    .Text = gName(rower).ToUpper + ", " + sName(rower),
+                    .Font = New Drawing.Font("Segoe UI", 8),
                     .Location = New Point(0, 20),
-                    .Name = "rName" + x.ToString
+                    .Name = "rName" + rower.ToString
                 }
+            If testName.Text.Length > 15 Then
+                testName.Text = gName(rower).ToUpper + ", "
+                Dim testName2 As New Label With
+                {
+                    .Text = sName(rower),
+                    .Font = New Drawing.Font("Segoe UI", 8),
+                    .Location = New Point(0, 33),
+                    .Name = "rName2" + rower.ToString
+                }
+                testPanel.Controls.Add(testName2)
+                'AddHandler testName2.MouseClick, AddressOf RowerPanelClicked
+            End If
+            'AddHandler testPanel.MouseClick, AddressOf RowerPanelClicked
+            AddHandler testPanel.MouseEnter, AddressOf RowerPanelEnter
+            AddHandler testPanel.MouseLeave, AddressOf RowerPanelExit
+            'AddHandler testID.MouseClick, AddressOf RowerPanelClicked
+            'AddHandler testName.MouseClick, AddressOf RowerPanelClicked
             RowerBox.Controls.Add(testPanel)
             testPanel.Controls.Add(testID)
             testPanel.Controls.Add(testName)
+            testPanel.BringToFront()
         Next
-
-
-
     End Sub
 End Class
