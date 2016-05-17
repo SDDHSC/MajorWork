e1136f25fc6db47c47a4854c526e24152c253e0c
OtherBarry <charlie.wilson4@gmail.com>
2016-05-17 21:47:10 +1000

    Profiles Done except Visuals and Filter/Sort

--- MajorWork/MajorWork/ProfilesView.Designer.vb
+++ MajorWork/MajorWork/ProfilesView.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class ProfilesView
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,67 +20,287 @@ Partial Class ProfilesView
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.RichTextBox()
         Me.RowerBox = New System.Windows.Forms.FlowLayoutPanel()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblRaAtPc = New System.Windows.Forms.Label()
+        Me.lblTrAtPc = New System.Windows.Forms.Label()
+        Me.lblPosition = New System.Windows.Forms.Label()
+        Me.lblSide = New System.Windows.Forms.Label()
+        Me.lblBeep = New System.Windows.Forms.Label()
+        Me.lbl2k = New System.Windows.Forms.Label()
+        Me.lblWeight = New System.Windows.Forms.Label()
+        Me.lblrClass = New System.Windows.Forms.Label()
+        Me.lblGroup = New System.Windows.Forms.Label()
+        Me.lblsName = New System.Windows.Forms.Label()
+        Me.lblfName = New System.Windows.Forms.Label()
+        Me.lblID = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.LinkLabel()
         Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(485, 200)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(232, 179)
-        Me.TextBox1.TabIndex = 1
-        Me.TextBox1.Text = ""
         '
         'RowerBox
         '
         Me.RowerBox.AutoScroll = True
-        Me.RowerBox.Location = New System.Drawing.Point(12, 12)
+        Me.RowerBox.Location = New System.Drawing.Point(12, 39)
         Me.RowerBox.Name = "RowerBox"
-        Me.RowerBox.Size = New System.Drawing.Size(467, 367)
+        Me.RowerBox.Size = New System.Drawing.Size(467, 340)
         Me.RowerBox.TabIndex = 1
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.lblEmail)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.lblRaAtPc)
+        Me.Panel1.Controls.Add(Me.lblTrAtPc)
+        Me.Panel1.Controls.Add(Me.lblPosition)
+        Me.Panel1.Controls.Add(Me.lblSide)
+        Me.Panel1.Controls.Add(Me.lblBeep)
+        Me.Panel1.Controls.Add(Me.lbl2k)
+        Me.Panel1.Controls.Add(Me.lblWeight)
+        Me.Panel1.Controls.Add(Me.lblrClass)
+        Me.Panel1.Controls.Add(Me.lblGroup)
+        Me.Panel1.Controls.Add(Me.lblsName)
+        Me.Panel1.Controls.Add(Me.lblfName)
+        Me.Panel1.Controls.Add(Me.lblID)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(485, 39)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(232, 340)
+        Me.Panel1.TabIndex = 2
+        '
+        'lblRaAtPc
+        '
+        Me.lblRaAtPc.AutoSize = True
+        Me.lblRaAtPc.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lblRaAtPc.Location = New System.Drawing.Point(3, 277)
+        Me.lblRaAtPc.Name = "lblRaAtPc"
+        Me.lblRaAtPc.Size = New System.Drawing.Size(172, 21)
+        Me.lblRaAtPc.TabIndex = 12
+        Me.lblRaAtPc.Text = "Race Attendance: 100%"
+        '
+        'lblTrAtPc
+        '
+        Me.lblTrAtPc.AutoSize = True
+        Me.lblTrAtPc.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lblTrAtPc.Location = New System.Drawing.Point(3, 256)
+        Me.lblTrAtPc.Name = "lblTrAtPc"
+        Me.lblTrAtPc.Size = New System.Drawing.Size(195, 21)
+        Me.lblTrAtPc.TabIndex = 11
+        Me.lblTrAtPc.Text = "Training Attendance: 100%"
+        '
+        'lblPosition
+        '
+        Me.lblPosition.AutoSize = True
+        Me.lblPosition.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lblPosition.Location = New System.Drawing.Point(3, 235)
+        Me.lblPosition.Name = "lblPosition"
+        Me.lblPosition.Size = New System.Drawing.Size(184, 21)
+        Me.lblPosition.TabIndex = 10
+        Me.lblPosition.Text = "Preferred Position: 3 Seat"
+        '
+        'lblSide
+        '
+        Me.lblSide.AutoSize = True
+        Me.lblSide.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lblSide.Location = New System.Drawing.Point(3, 214)
+        Me.lblSide.Name = "lblSide"
+        Me.lblSide.Size = New System.Drawing.Size(160, 21)
+        Me.lblSide.TabIndex = 9
+        Me.lblSide.Text = "Preferred Side: Stroke"
+        '
+        'lblBeep
+        '
+        Me.lblBeep.AutoSize = True
+        Me.lblBeep.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lblBeep.Location = New System.Drawing.Point(3, 193)
+        Me.lblBeep.Name = "lblBeep"
+        Me.lblBeep.Size = New System.Drawing.Size(111, 21)
+        Me.lblBeep.TabIndex = 8
+        Me.lblBeep.Text = "Beep Test: 12.4"
+        '
+        'lbl2k
+        '
+        Me.lbl2k.AutoSize = True
+        Me.lbl2k.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lbl2k.Location = New System.Drawing.Point(3, 172)
+        Me.lbl2k.Name = "lbl2k"
+        Me.lbl2k.Size = New System.Drawing.Size(137, 21)
+        Me.lbl2k.TabIndex = 7
+        Me.lbl2k.Text = "2km Time: 6:23:45"
+        '
+        'lblWeight
+        '
+        Me.lblWeight.AutoSize = True
+        Me.lblWeight.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWeight.Location = New System.Drawing.Point(3, 151)
+        Me.lblWeight.Name = "lblWeight"
+        Me.lblWeight.Size = New System.Drawing.Size(101, 21)
+        Me.lblWeight.TabIndex = 6
+        Me.lblWeight.Text = "Weight: 60kg"
+        '
+        'lblrClass
+        '
+        Me.lblrClass.AutoSize = True
+        Me.lblrClass.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lblrClass.Location = New System.Drawing.Point(119, 96)
+        Me.lblrClass.Name = "lblrClass"
+        Me.lblrClass.Size = New System.Drawing.Size(79, 21)
+        Me.lblrClass.TabIndex = 5
+        Me.lblrClass.Text = "Class: 12E"
+        '
+        'lblGroup
+        '
+        Me.lblGroup.AutoSize = True
+        Me.lblGroup.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lblGroup.Location = New System.Drawing.Point(119, 127)
+        Me.lblGroup.Name = "lblGroup"
+        Me.lblGroup.Size = New System.Drawing.Size(108, 21)
+        Me.lblGroup.TabIndex = 4
+        Me.lblGroup.Text = "Year 10 Eights"
+        '
+        'lblsName
+        '
+        Me.lblsName.AutoSize = True
+        Me.lblsName.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lblsName.Location = New System.Drawing.Point(119, 62)
+        Me.lblsName.Name = "lblsName"
+        Me.lblsName.Size = New System.Drawing.Size(72, 21)
+        Me.lblsName.TabIndex = 3
+        Me.lblsName.Text = "Manning"
+        '
+        'lblfName
+        '
+        Me.lblfName.AutoSize = True
+        Me.lblfName.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lblfName.Location = New System.Drawing.Point(119, 33)
+        Me.lblfName.Name = "lblfName"
+        Me.lblfName.Size = New System.Drawing.Size(82, 21)
+        Me.lblfName.TabIndex = 2
+        Me.lblfName.Text = "BLACKALL"
+        '
+        'lblID
+        '
+        Me.lblID.AutoSize = True
+        Me.lblID.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lblID.Location = New System.Drawing.Point(119, 3)
+        Me.lblID.Name = "lblID"
+        Me.lblID.Size = New System.Drawing.Size(113, 21)
+        Me.lblID.TabIndex = 1
+        Me.lblID.Text = "ID: 430227866"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.MajorWork.My.Resources.Resources.BlankProfilePictuer
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(110, 145)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(47, 12)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(180, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(299, 12)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(180, 21)
+        Me.ComboBox2.TabIndex = 4
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 8.0!)
-        Me.Label1.Location = New System.Drawing.Point(4, 18)
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(136, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "SHIVAKUMAARUN, Kieran"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Sort:"
         '
-        'Panel1
+        'Label2
         '
-        Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Location = New System.Drawing.Point(534, 75)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(140, 50)
-        Me.Panel1.TabIndex = 2
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(261, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(32, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Filter:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.Label3.Location = New System.Drawing.Point(3, 298)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 21)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Email:"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(37, 319)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(190, 13)
+        Me.lblEmail.TabIndex = 15
+        Me.lblEmail.TabStop = True
+        Me.lblEmail.Text = "430227866@student.sbhs.nsw.edu.au"
         '
         'ProfilesView
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(729, 391)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.RowerBox)
-        Me.Controls.Add(Me.TextBox1)
         Me.Name = "ProfilesView"
         Me.Text = "Form1"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents RowerBox As FlowLayoutPanel
-    Friend WithEvents TextBox1 As RichTextBox
-    Friend WithEvents Label1 As Label
     Friend WithEvents Panel1 As Panel
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblRaAtPc As Label
+    Friend WithEvents lblTrAtPc As Label
+    Friend WithEvents lblPosition As Label
+    Friend WithEvents lblSide As Label
+    Friend WithEvents lblBeep As Label
+    Friend WithEvents lbl2k As Label
+    Friend WithEvents lblWeight As Label
+    Friend WithEvents lblrClass As Label
+    Friend WithEvents lblGroup As Label
+    Friend WithEvents lblsName As Label
+    Friend WithEvents lblfName As Label
+    Friend WithEvents lblID As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblEmail As LinkLabel
+    Friend WithEvents Label3 As Label
 End Class
--- MajorWork/MajorWork/ProfilesView.vb
+++ MajorWork/MajorWork/ProfilesView.vb
@@ -1,28 +1,19 @@
 ﻿Imports System.Data.OleDb
 Public Class ProfilesView
-    Private Sub RowerPanelEnter(sender As Object, e As EventArgs)
-        Dim entered As Panel = sender
-        entered.BackColor = skyOrange
-    End Sub
-    Private Sub RowerPanelExit(sender As Object, e As EventArgs)
-        Dim exited As Panel = sender
-        exited.BackColor = schoolBlue
-    End Sub
+    Dim IDNum As New List(Of Int32)
+    Dim IDStr As New List(Of String)
+    Dim gName As New List(Of String)
+    Dim sName As New List(Of String)
+    Dim yGroup As New List(Of String)
+    Dim rClass As New List(Of String)
     Private Sub ProfilesView_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Dim SelectedID As Integer = 400052841
-        Dim IDNum As New List(Of Int32)
-        Dim IDStr As New List(Of String)
-        Dim gName As New List(Of String)
-        Dim sName As New List(Of String)
-        Dim yGroup As New List(Of String)
-        Dim rClass As New List(Of String)
+        'Read Database
         Using conn As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\rowingDatabase (1).accdb")
             conn.Open()
             Using cmd As New OleDbCommand("SELECT ID, sName, gName, rClass, Group FROM tbProfiles", conn)
                 Using dr = cmd.ExecuteReader()
                     If dr.HasRows Then
                         Do While dr.Read()
-                            TextBox1.Text += dr.GetInt32(0).ToString + "    " + dr.GetString(1) + " " + dr.GetString(2) & vbCrLf
                             IDNum.Add(dr.GetInt32(0))
                             IDStr.Add(dr.GetInt32(0).ToString)
                             gName.Add(dr.GetString(2))
@@ -34,13 +25,15 @@ Public Class ProfilesView
                 End Using
             End Using
         End Using
+        'Create Panels
         For rower As Integer = 0 To IDNum.Count - 1
             Dim testPanel As New Panel With
                 {
                     .Height = 50,
                     .Width = 140,
                     .BackColor = schoolBlue,
-                    .Name = "rPanel" + rower.ToString
+                    .Name = rower.ToString,
+                    .Tag = "NotCLicked"
                 }
             Dim testID As New Label With
                 {
@@ -66,17 +59,85 @@ Public Class ProfilesView
                     .Name = "rName2" + rower.ToString
                 }
                 testPanel.Controls.Add(testName2)
-                'AddHandler testName2.MouseClick, AddressOf RowerPanelClicked
+                AddHandler testName2.MouseClick, AddressOf RowerPanelTextClicked
+                AddHandler testName2.MouseEnter, AddressOf RowerPanelTextEnter
+                AddHandler testName2.MouseLeave, AddressOf RowerPanelTextExit
             End If
-            'AddHandler testPanel.MouseClick, AddressOf RowerPanelClicked
+            AddHandler testPanel.MouseClick, AddressOf RowerPanelClicked
+            AddHandler testID.MouseClick, AddressOf RowerPanelTextClicked
+            AddHandler testName.MouseClick, AddressOf RowerPanelTextClicked
             AddHandler testPanel.MouseEnter, AddressOf RowerPanelEnter
             AddHandler testPanel.MouseLeave, AddressOf RowerPanelExit
-            'AddHandler testID.MouseClick, AddressOf RowerPanelClicked
-            'AddHandler testName.MouseClick, AddressOf RowerPanelClicked
+            AddHandler testName.MouseEnter, AddressOf RowerPanelTextEnter
+            AddHandler testName.MouseLeave, AddressOf RowerPanelTextExit
+            AddHandler testID.MouseEnter, AddressOf RowerPanelTextEnter
+            AddHandler testID.MouseLeave, AddressOf RowerPanelTextExit
             RowerBox.Controls.Add(testPanel)
             testPanel.Controls.Add(testID)
             testPanel.Controls.Add(testName)
-            testPanel.BringToFront()
         Next
+    End Sub
+    'Highlighting
+    Private Sub RowerPanelEnter(sender As Object, e As EventArgs)
+        If sender.Tag <> "Clicked" Then
+            Dim entered As Panel = sender
+            entered.BackColor = skyYellow
+        End If
+    End Sub
+    Private Sub RowerPanelTextEnter(sender As Object, e As EventArgs)
+        RowerPanelEnter(sender.Parent, e)
+    End Sub
+    Private Sub RowerPanelExit(sender As Object, e As EventArgs)
+        Dim exited As Panel = sender
+        If exited.Tag <> "Clicked" Then
+            exited.BackColor = schoolBlue
+        End If
+    End Sub
+    Private Sub RowerPanelTextExit(sender As Object, e As EventArgs)
+        RowerPanelExit(sender.Parent, e)
+    End Sub
+    'Click
+    Private Sub RowerPanelTextClicked(sender As Object, e As EventArgs)
+        RowerPanelClicked(sender.Parent, e)
+    End Sub
+    Private Sub RowerPanelClicked(sender As Object, e As EventArgs)
+        For Each i As Panel In RowerBox.Controls
+            i.Tag = "NotClicked"
+            i.BackColor = schoolBlue
+        Next
+        Dim clicked As Panel = sender
+        clicked.Tag = "Clicked"
+        clicked.BackColor = skyOrange
+        Dim index = CInt(clicked.Name)
+        FillInfo(index)
+    End Sub
+    Private Sub FillInfo(index As Integer)
+        lblID.Text = "ID: " + IDStr(index)
+        lblsName.Text = gName(index)
+        lblfName.Text = sName(index)
+        lblrClass.Text = "Class: " + rClass(index)
+        Select Case yGroup(index)
+            Case "8"
+                lblGroup.Text = "Year 8 Quads"
+            Case "9"
+                lblGroup.Text = "Year 9 Quads"
+            Case "10"
+                lblGroup.Text = "Year 10 Eights"
+            Case "1"
+                lblGroup.Text = "1st VIII"
+        End Select
+        Randomize()
+        lblWeight.Text = "Weight: " + CInt(Int((60 * Rnd()) + 40)).ToString + "kg"
+        Randomize()
+        lbl2k.Text = "2km Time: " + "0" + CInt(Int((6 * Rnd()) + 4)).ToString + ":" + CInt(Int((59 * Rnd()) + 1)).ToString
+        Randomize()
+        lblBeep.Text = "Beep Test Score: " + CDec((Int((100 * Rnd()) + 40)) / 10).ToString
+        lblPosition.Text = "Preferred Side: Stroke"
+        lblSide.Text = "Preferred Position: Stroke"
+        Randomize()
+        lblTrAtPc.Text = "Training Attendance: " + CInt(Int((100 * Rnd()) + 1)).ToString + "%"
+        Randomize()
+        lblRaAtPc.Text = "Race Attendance: " + CInt(Int((100 * Rnd()) + 1)).ToString + "%"
+        lblEmail.Text = IDStr(index) + "@student.sbhs.nsw.edu.au"
     End Sub
 End Class
