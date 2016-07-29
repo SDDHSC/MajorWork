f748ec58d1f451f61caf8883b5370e84643112a8
Ricky Pachon <rickypachon@gmail.com>
2016-07-29 11:09:58 +1000

    All modules combined
    database fixed and shit

--- MajorWork/MajorWork/ForgotPassword.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class ForgotPassword
-    Private Sub back_Click(sender As Object, e As EventArgs) Handles back.Click
-        Login.Show()
-        Me.Close()
-    End Sub
-End Class
--- MajorWork/MajorWork/Options.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Options
-    Private Sub Options_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        styleForm(Me)
-
-    End Sub
-End Class
